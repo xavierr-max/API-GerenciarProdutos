e1c558830f2d1d000604ea6023c464097ac73984
Maxwell <maxwelldearaujo1017@gmail.com>
2025-05-15 22:47:07 -0300

    V3

--- GerenciarProdutos/Configuration.cs
+++ GerenciarProdutos/Configuration.cs
@@ -2,7 +2,7 @@
 
 public static class Configuration
 {
-    public static string? JwtKey { get; set; } = "ZXh0cmVtZWx5LXNlY3VyZS1rZXktZm9yLWp3dC1hdXRoZW50aWNhdGlvbi1taW5pbXVtLTMyLWNoYXJhY3RlcnM=";
+    public static string? JwtKey { get; set; } = "CALMA CALABRESO";
     public static SmtpConfiguration Smtp = new();
     public class SmtpConfiguration
     {
